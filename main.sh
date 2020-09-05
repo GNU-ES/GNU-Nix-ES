e1e59b37f7b379ec3554f251b5f81d327d3b8ab6
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-05 05:31:13 -0300

    Adds alpine-nix-install-ex1

--- main.sh
+++ main.sh
@@ -6,3 +6,6 @@ set -euxo pipefail
 ./run.sh 'src/examples/nix/FiqueEmCasaConf/nix-gcc-9.2.0-centos-5.11'
 ./run.sh 'src/examples/nix/nix-in-docker/nix-install/ubuntu-12-04-adduser-nix-install-sudo'
 ./run.sh 'src/examples/nix/FiqueEmCasaConf/sizes/size-nix-from-nixpkgs'
+./run.sh 'src/examples/nix/nix-in-docker/nix-install/alpine-nix-install/ex1'
+#./run.sh ''
+
