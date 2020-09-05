5a978c58178f4929c4c7881723a1f8ab388a1ae5
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-05 18:03:33 -0300

    Forgotten file

--- main.sh
+++ main.sh
@@ -7,5 +7,6 @@ set -euxo pipefail
 ./run.sh 'src/examples/nix/nix-in-docker/nix-install/ubuntu-12-04-adduser-nix-install-sudo'
 ./run.sh 'src/examples/nix/FiqueEmCasaConf/sizes/size-nix-from-nixpkgs'
 ./run.sh 'src/examples/nix/nix-in-docker/nix-install/alpine-nix-install/ex1'
+./run.sh 'src/examples/ex1-hello-tweag'
 #./run.sh ''
 
