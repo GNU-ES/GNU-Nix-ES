244613d4987c06ab383712947b28f2bec2af7544
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-06 17:33:35 -0300

    Adds nix-in-docker-ubuntu-20-04-adduser-nix-install-sudo-tested-python3

---
+++ src/examples/nix-in-docker-ubuntu-20-04-adduser-nix-install-sudo-tested-python3/run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+IMAGE='gnu-nix-es/ubuntu-12-04-adduser-nix-install-sudo'
+VERSION=0.0.1
+
+IMAGE_VERSION="$IMAGE":"$VERSION"
+
+docker build --tag "$IMAGE_VERSION" .
+
+# Usefull for debug:
+# echo -e " "'"$ENV->"'"$ENV\n" '"$NIX_PATH"->'"$NIX_PATH\n" '"$PATH"->'"$PATH\n" '"$USER"->' "$USER\n"
+
+#docker run \
+#--interactive \
+#--rm \
+#--tty \
+#--user pedro \
+#"$IMAGE_VERSION" \
+#bash -c "nix --version && nix-env --install --attr nixpkgs.python39 && python --version && python -c 'print(12345)'"
+
+
+docker run \
+--tty \
+--user pedro \
+"$IMAGE_VERSION" \
+bash -c "nix --version && nix-env --install --attr nixpkgs.python39 && python --version && python -c 'print(12345)'"
