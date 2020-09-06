0506c70d905bc3349a6a339826026f81110ce156
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-05 23:46:09 -0300

    Auto stash before cherrypick of "Improvements, works ok" a zilion

---
+++ src/examples/WIP-Work-In-Process/nix/nix-in-docker/nix-install/debian-slim-adduser-nix-install-sudo-flake-fixperms/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+IMAGE='gnu-nix-es/debian-slim-adduser-nix-install-sudo-flake-fixperms'
+VERSION=0.0.1
+
+IMAGE_VERSION="$IMAGE":"$VERSION"
+
+docker build --tag "$IMAGE_VERSION" .
+
+#echo -e " "'"$ENV->"'"$ENV\n" '"$NIX_PATH"->'"$NIX_PATH\n" '"$PATH"->'"$PATH\n" '"$USER"->' "$USER\n"
+
+docker run \
+--interactive \
+--rm \
+--tty \
+--user pedro \
+"$IMAGE_VERSION" --run 'nix flake show github:GNU-ES/hello'
+
