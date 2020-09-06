9ec238e933a5136c0be9e0bf8d766cb293b2b9dd
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-06 01:05:57 -0300

    Test cherry pick 3

---
+++ src/examples/ex1-hello-tweag/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+#set -euxo pipefail
+
+IMAGE='gnu-nix-es/ex1-hello-tweag'
+VERSION=0.0.1
+
+IMAGE_VERSION="$IMAGE":"$VERSION"
+
+docker build --tag "$IMAGE_VERSION" .
+
+docker run \
+--interactive \
+--tty \
+--rm \
+"$IMAGE_VERSION" --run 'nix flake show github:GNU-ES/hello'
+
+
+docker run \
+--interactive \
+--tty \
+--rm \
+--workdir /code \
+--volume "$(pwd)":/code \
+"$IMAGE_VERSION" --run './tweag-tutorial.sh'
+
