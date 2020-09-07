e8ef776261af3229b694b94e097f6e5896f1bd88
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-07 03:52:39 -0300

    Test

--- src/examples/ex1-hello-tweag/user-run.sh
+++ src/examples/ex1-hello-tweag/user-run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-sudo apt-get update -y \
-sudo apt-get install -y \
-curl \
-&& curl cfgcfg
+git clone https://github.com/GNU-ES/GNU-Nix-ES.git \
+&& cd GNU-Nix-ES \
+&& git checkout REVISION \
+&& cd src/examples/ex1-hello-tweag \
+&& ./run.sh
+
+
+
+#sudo apt-get update -y \
+#sudo apt-get install -y \
+#curl \
+#&& curl cfgcfg
