ebb2fad05f672ee9d8c3f2d3bb05c4aac401f253
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-07 02:18:31 -0300

    push

--- src/examples/ex1-hello-tweag/user-run.sh
+++ src/examples/ex1-hello-tweag/user-run.sh
@@ -1,6 +1,5 @@
-git clone https://github.com/GNU-ES/GNU-Nix-ES.git \
-&& cd GNU-Nix-ES \
-&& git checkout 7e2be43e22149a2414d5f84212c0a28f6588f778 \
-&& cd src/examples/ex1-hello-tweag \
-&& ./run.sh \
-&& ./end-mensage.sh
+#!/usr/bin/env bash
+sudo apt-get update -y \
+sudo apt-get install -y \
+curl \
+&& curl cfgcfg
