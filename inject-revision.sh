a4b0a8d762bf7245efa567fc5151ad3fca2c5823
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-07 04:31:36 -0300

    Just save

--- inject-revision.sh
+++ inject-revision.sh
@@ -3,10 +3,21 @@
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 #set -euxo pipefail
 
-cd src/examples
+#cd src/examples
+#
+#find . -name 'user-run.sh' -exec sed --expression="s:REVISION:$(git rev-parse $(git rev-parse --short HEAD)):g" --in-place  {} +
+#
 
-find . -name 'user-run.sh' -exec sed --expression="s:REVISION:$(git rev-parse $(git rev-parse --short HEAD)):g" --in-place  {} +
-find . -name 'README.md' -exec sed "s/user-run.sh/"$(cat {})"/"
+function substitute_BLOCK_with_FILEcontents {
+  local BLOCK_StartRegexp="${1}"
+  local BLOCK_EndRegexp="${2}"
+  local FILE="${3}"
+  sed -e "/${BLOCK_EndRegexp}/a ___tmpMark___" -e "/${BLOCK_StartRegexp}/,/${BLOCK_EndRegexp}/d" | sed -e "/___tmpMark___/r ${FILE}" -e '/___tmpMark___/d'
+}
+
+
+
+#find . -name 'README.md' -exec substitute_BLOCK_with_FILEcontents {} +
 
 
 
