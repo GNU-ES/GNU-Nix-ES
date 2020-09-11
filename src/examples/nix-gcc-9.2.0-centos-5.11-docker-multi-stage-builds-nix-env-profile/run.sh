540038d18815f1303f1f89766e0765fcb519d2c0
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-10 23:58:26 -0300

    More 3

--- src/examples/nix-gcc-9.2.0-centos-5.11-docker-multi-stage-builds-nix-env-profile/run.sh
+++ src/examples/nix-gcc-9.2.0-centos-5.11-docker-multi-stage-builds-nix-env-profile/run.sh
@@ -2,7 +2,7 @@
 
 
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
-set -euxo pipefail
+set -eux pipefail
 
 IMAGE="gnu-nix-es/$(git rev-parse --short HEAD)"
 VERSION=0.0.1
