77a70f60ef54e760f4ec10ffb89773eebe01bef2
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-13 20:15:38 -0300

    save

--- src/to-run-in-github-action/inject-revision.sh
+++ src/to-run-in-github-action/inject-revision.sh
@@ -9,5 +9,8 @@ cd 'to-be-moved'
 
 find . -type f -name "README.md" -exec sed --in-place --regexp-extended "s/\b([a-f0-9]{40})\b/"$REVISION"/g" {} +
 
+FOLDER_NAME="$(basename "$(pwd)")"
+#PIECE=${FOLDER_NAME}' \\'
+#find . -type f -name "README.md" -exec sed --in-place --regexp-extended "s/&& cd src\/examples[^ ]*/\&\& cd src\/examples\/"$PIECE"/g" {} +
 
-find . -type f -name "README.md" -exec sed --in-place --regexp-extended "s/&& cd src\/examples[^ ]*/\&\& cd src\/examples\/"$REVISION"/g" {} +
+find . -type f -name "README.md" -exec sed --in-place --regexp-extended "s/&& cd src\/examples[^ ]*/\&\& cd src\/examples\/"$FOLDER_NAME"/g" {} +
--- src/to-run-in-github-action/to-be-moved/testing/README.md
+++ src/to-run-in-github-action/to-be-moved/testing/README.md
@@ -3,8 +3,8 @@
 #!/usr/bin/env bash
 git clone https://github.com/GNU-ES/GNU-Nix-ES.git \
 && cd GNU-Nix-ES \
-&& git checkout 2192bf9fd983b70a692be0541ddc3f583e327a72 \
-&& cd src/examples/alpine-nix-install-in-Dockerfile-ex1 \
+&& git checkout 0e7f378a07ec194211318dc1705be3796c280f81 \
+&& cd src/examples/to-run-in-github-action \
 && ./run.sh
 
 WORD1 WORD2 WORD3 BLA BLA WORD1 WORD4 WORD3
