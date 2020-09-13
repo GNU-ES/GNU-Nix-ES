c33327421779bd28be204b36dc2e2b84d44ecdb6
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-13 19:19:35 -0300

    Save

--- src/to-run-in-github-action/in-action.sh
+++ src/to-run-in-github-action/in-action.sh
@@ -20,7 +20,7 @@
 
 REVISION=$(git rev-parse $(git rev-parse --short HEAD))
 
-find . -type f -name "README.md" -exec sed --in-place --regexp-extended 's/\b([a-f0-9]{40})\b/$REVISION/g' {} +
+find . -type f -name "README.md" -exec sed --in-place --regexp-extended 's/\b([a-f0-9]{40})\b/"$REVISION"/g' {} +
 
 
 #find . -name 'README.md' -exec {} \;
