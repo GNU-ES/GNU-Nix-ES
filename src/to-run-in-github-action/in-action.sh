357cb9e7565c2cc3e96cb0162a97e2275a36a0be
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-13 21:15:46 -0300

    wip

--- src/to-run-in-github-action/in-action.sh
+++ src/to-run-in-github-action/in-action.sh
@@ -3,8 +3,10 @@
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 #set -euxo pipefail
 
-echo pwd
-
+echo "$(pwd)"
+cd src/to-run-in-github-action/
+echo "$(pwd)"
+#."$(pwd)"/to-run-in-github-action/check-run.sh
 ./check-run.sh
 
 ./inject-revision-and-folder-name.sh
