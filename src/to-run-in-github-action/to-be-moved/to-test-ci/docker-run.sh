4443b12490aedded3da0ef0253dfe94f7e1c70db
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-14 20:35:55 -0300

    More debug

--- src/to-run-in-github-action/to-be-moved/to-test-ci/docker-run.sh
+++ src/to-run-in-github-action/to-be-moved/to-test-ci/docker-run.sh
@@ -6,6 +6,14 @@
 
 ./docker-build.sh
 
+exit_code=$?
+echo 'The exite code:'$exit_code
+
+if [ $exit_code -eq 1 ] ; then
+  echo 'The ./run.sh seens to have worked correcly.'
+else
+  echo 'The ./run.sh may have failed.'
+fi
 
 IMAGE="gnu-nix-es/$(git rev-parse --short HEAD)"
 VERSION=0.0.1
