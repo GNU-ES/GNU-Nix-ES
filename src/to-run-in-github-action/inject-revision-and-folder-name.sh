dd7b776652189904a80e17eb625b66939eab9c5c
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-14 02:34:28 -0300

    Fix

--- src/to-run-in-github-action/inject-revision-and-folder-name.sh
+++ src/to-run-in-github-action/inject-revision-and-folder-name.sh
@@ -6,9 +6,9 @@
 REVISION=$(git rev-parse $(git rev-parse --short HEAD))
 
 #ls -la
-cd "$1"/${2:-''}
+#cd "$1"/${2:-''}
 #https://stackoverflow.com/a/3601734
-#echo "$1"${2:-''}
+echo "$1"${2:-''}
 
 find . -type f -name "README.md" -exec sed --in-place --regexp-extended "s/\b([a-f0-9]{40})\b/"$REVISION"/g" {} +
 
