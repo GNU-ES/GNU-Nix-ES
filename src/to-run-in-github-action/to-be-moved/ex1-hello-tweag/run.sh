9f6e92957b5940bdfab70076b405fe756a47045d
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-14 03:26:07 -0300

    test

--- src/to-run-in-github-action/to-be-moved/ex1-hello-tweag/run.sh
+++ src/to-run-in-github-action/to-be-moved/ex1-hello-tweag/run.sh
@@ -6,7 +6,7 @@ set -eux pipefail
 IMAGE="gnu-nix-es/$(git rev-parse --short HEAD)"
 VERSION=0.0.1
 IMAGE_VERSION="$IMAGE":"$VERSION"
-
+xpto
 #docker build \
 #--label org.opencontainers.image.created=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
 #--label org.opencontainers.image.revision=$(git rev-parse $(git rev-parse --short HEAD)) \
