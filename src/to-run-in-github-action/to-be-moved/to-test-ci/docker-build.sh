fffb5d784a559c9842305d980f8ed9470c2bd884
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-14 20:57:13 -0300

    Must fail

--- src/to-run-in-github-action/to-be-moved/to-test-ci/docker-build.sh
+++ src/to-run-in-github-action/to-be-moved/to-test-ci/docker-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 ## See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
-set -euxo pipefail
+set -eux pipefail
 
 
 IMAGE="gnu-nix-es/$(git rev-parse --short HEAD)"
