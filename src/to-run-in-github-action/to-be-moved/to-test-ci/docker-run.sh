e0e6d4d2ec407e947b2be996e99bb722bccb582c
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-14 20:28:55 -0300

    Refactoring

--- src/to-run-in-github-action/to-be-moved/to-test-ci/docker-run.sh
+++ src/to-run-in-github-action/to-be-moved/to-test-ci/docker-run.sh
@@ -4,17 +4,12 @@
 #set -eux pipefail
 
 
+./docker-build.sh
+
+
 IMAGE="gnu-nix-es/$(git rev-parse --short HEAD)"
 VERSION=0.0.1
 IMAGE_VERSION="$IMAGE":"$VERSION"
-
-
-docker build \
---label org.opencontainers.image.created=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
---label org.opencontainers.image.revision=$(git rev-parse $(git rev-parse --short HEAD)) \
---tag \
-"$IMAGE_VERSION" .
-
 
 docker run \
 --interactive \
