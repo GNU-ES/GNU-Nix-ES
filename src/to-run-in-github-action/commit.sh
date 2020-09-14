d71eee90585f497c507d325878f5092257a3b1cb
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-14 00:17:19 -0300

    git config tesy

--- src/to-run-in-github-action/commit.sh
+++ src/to-run-in-github-action/commit.sh
@@ -3,6 +3,9 @@
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 #set -euxo pipefail
 
+git config --global user.email "pedroalencarregis@hotmail.com"
+git config --global user.name "PedroRegisPOAR"
+
 git add .
 
 git commit --message "Add example:$1"
