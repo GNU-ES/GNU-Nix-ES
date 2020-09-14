4f302cf3650421ca6831a5625b829528c90303c2
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-13 21:36:25 -0300

    Fix mv

--- src/to-run-in-github-action/move-to-examples.sh
+++ src/to-run-in-github-action/move-to-examples.sh
@@ -8,4 +8,10 @@ cd to-be-moved
 # https://stackoverflow.com/a/10605775
 FOLDER_NAME="$(find . -mindepth 1 -maxdepth 1 -type d | cut --delimiter='/' --field=2)"
 
-mv "$FOLDER_NAME" ../../examples/
+if [ -n $FOLDER_NAME ]; then
+  echo "The FOLDER_NAME variable is empt."
+  echo "So, nothing to move!"
+else
+  echo "Moving "$FOLDER_NAME" to examples."
+  mv "$FOLDER_NAME" ../../examples/
+fi
