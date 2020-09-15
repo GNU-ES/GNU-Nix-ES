b0008e78e451ea02701a157e047155a2d47806e2
Pedro Regis <pedroalencarregis@hotmail.com>
2020-09-15 00:18:40 -0300

    Testing

--- src/to-run-in-github-action/check-run.sh
+++ src/to-run-in-github-action/check-run.sh
@@ -3,11 +3,18 @@
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 #set -eux pipefail
 
-echo "$(pwd)"
-ls -la
-cd to-be-moved/"$1"
-echo "$(pwd)"
-ls -la
+#echo "$(pwd)"
+#ls -la
+#cd to-be-moved/"$1"
+#echo "$(pwd)"
+#ls -la
+#
+#./run.sh
 
-./run.sh
+
+ls -la
+echo '###############################'
+ls -la to-be-moved/"$1"
+
+./to-be-moved/"$1"/run.sh
 
