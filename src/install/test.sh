9e681440a68cf9d1bc888d1aecd1eb8e0589087a
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-09 20:22:07 -0300

    WIP

--- src/install/test.sh
+++ src/install/test.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 declare -i FOOBAR=12;
 echo $FOOBAR;
+
+docker run \
+--interactive \
+--rm \
+--tty \
+ubuntu:20.04 \
+bash -c "apt-get update \
+&& apt-get install -y git \
+&& git clone https://github.com/GNU-ES/GNU-Nix-ES.git \
+&& cd GNU-Nix-ES \
+&& git checkout 7d544bc4c931f49ef0a2ae0e6aaeeb1532ee6e2d \
+&& cd src/install \
+&& ./run.sh"
+nix shell github:GNU-ES/hello -c hello
