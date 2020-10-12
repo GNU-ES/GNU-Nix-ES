a7a5fd4a1b5e9143c1359a3e686b21f10f4a9170
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-12 19:19:28 -0300

    Corrections, nothing so big

--- src/WIP-Work-In-Process/NYLUG-Graham-Christensen-dockerTools/ex2/run.sh
+++ src/WIP-Work-In-Process/NYLUG-Graham-Christensen-dockerTools/ex2/run.sh
@@ -4,14 +4,12 @@
 set -euxo pipefail
 
 
-nix-build ./container-curl.nix
+nix-build ./curl-test.nix
 
 docker load < ./result
-
-docker run --interactive --rm --tty uname-test:0.0.1
 
 docker run --interactive --rm --tty curl-test:0.0.1 --help
 
 # TODO: bring thogether a server, for example, 
 # in python, a minimal http server to show that curl works
-docker run --interactive --rm --tty curl-test:0.0.1 google.com
+docker run --interactive --rm --tty curl-test:0.0.1 http://www.google.com/
