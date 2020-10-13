1f7afc332a84981e1970e255753ddb4404aca0cc
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-13 10:20:43 -0300

    WIP, misses tests for creation of user

---
+++ src/WIP-Work-In-Process/NYLUG-Graham-Christensen-dockerTools/ex5/bash-layered-with-user-with-sudo/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+nix-build ./bash-layered-with-user-with-sudo.nix
+
+docker load < ./result
+
+docker run --interactive --rm --tty bash-layered-with-user-with-sudo:0.0.1 bash -c 'ls -la'
+
+
+#    adduser \
+#    --disabled-password \
+#    --force-badname \
+#    --ingroup sudo \
+#    --quiet \
+#    --shell /bin/bash \
+#    --home /home/"$INPUTED_USER_OR_DEFAULT" \
+#    --gecos "User" "$INPUTED_USER_OR_DEFAULT"
+#
+#    useradd \
+#    --badnames \
+#    --gid sudo \
+#    --home-dir /home/"$INPUTED_USER_OR_DEFAULT"
+#    --shell /bin/bash \
+#    --no-user-group
