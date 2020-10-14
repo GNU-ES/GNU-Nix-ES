28b68db459d35dfec39df991ed88deae90077afe
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-14 09:46:01 -0300

    Add example docker-uname-scratch

---
+++ src/WIP-Work-In-Process/NYLUG-Graham-Christensen-dockerTools/docker-uname-scratch/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -eux pipefail
+
+IMAGE="gnu-nix-es/$(git rev-parse --short HEAD)"
+VERSION=0.0.1
+IMAGE_VERSION="$IMAGE":"$VERSION"
+
+
+docker build \
+--label org.opencontainers.image.created=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
+--label org.opencontainers.image.revision=$(git rev-parse $(git rev-parse --short HEAD)) \
+--tag \
+"$IMAGE_VERSION" .
+
+
+docker run \
+--interactive \
+--tty \
+"$IMAGE_VERSION"
