5457ecdb61dfae22afbc6f33a2c8bb9e45971047
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-19 19:59:04 -0300

    Add size example of texlive.combined.scheme-basic

---
+++ src/WIP-Work-In-Process/nix/FiqueEmCasaConf/sizes/texlive-basic-size-Dockerfile-COPY/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+# TODO use rp (ripgrep) to be able to use "$IMAGE_VERSION"
+SEARCH_NAME=sizes
+IMAGE='gnu-nix-es/'"$SEARCH_NAME"
+VERSION=0.0.1
+
+IMAGE_VERSION="$IMAGE":"$VERSION"
+
+docker build --tag "$IMAGE_VERSION" .
+
+docker images | grep "$SEARCH_NAME"
