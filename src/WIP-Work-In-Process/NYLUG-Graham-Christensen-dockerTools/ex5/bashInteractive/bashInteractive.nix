392f431c64423832b18f635385ef8c167617bcdf
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-19 20:25:18 -0300

    bashInteractive

---
+++ src/WIP-Work-In-Process/NYLUG-Graham-Christensen-dockerTools/ex5/bashInteractive/bashInteractive.nix
@@ -0,0 +1,15 @@
+let
+    pkgs = import <nixpkgs> {};
+in
+pkgs.dockerTools.buildLayeredImage {
+
+    name = "bash-interactive";
+    tag = "0.0.1";
+    created = "now";
+
+    contents = with pkgs; [
+        bashInteractive
+       ];
+
+    config.Entrypoint = [ "${pkgs.bashInteractive}/bin/bash" ];
+}
