9b52016bcb15058579f37d7e7b13f36e9c0ef6ed
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-23 22:23:51 -0300

    Works

--- src/WIP-Work-In-Process/nix/FiqueEmCasaConf/minimal-file-from-nix-to-container/Dockerfile-adduser-addgroup-sudo-wheel-COPY-alpine-dockerTools-3/nix-Luca Bruno-dockerTools.nix
+++ src/WIP-Work-In-Process/nix/FiqueEmCasaConf/minimal-file-from-nix-to-container/Dockerfile-adduser-addgroup-sudo-wheel-COPY-alpine-dockerTools-3/nix-Luca Bruno-dockerTools.nix
@@ -4,49 +4,8 @@ let
 
     entrypoint = pkgs.writeScript "entrypoint.sh" ''
     #!${pkgs.stdenv.shell}
-    ${pkgs.dockerTools.shadowSetup}
-
-    set -e
-
-    # allow the container to be started with `--user`
-    if [ "$(${pkgs.coreutils}/bin/id -u)" = "0" ]; then
-        #${pkgs.coreutils}/bin/chown -R redis .
-
-        # What are the differences in doing it here or in the runAsRoot?
-        #groupadd --gid 5000 app_group
-        #useradd --no-log-init --uid 5000 --gid app_group pedroregispoar
-
-        opt_u='pedroregispoar'
-        opt_g='app_group'
-        VOLUME_AND_WORKDIR='/code'
-
-        OLD_UID=$( ${pkgs.getent}/bin/getent passwd "$opt_u" | cut --field=3 --delimiter=:)
-        NEW_UID=$(stat --format="%u" $VOLUME_AND_WORKDIR)
-
-        if [ "$OLD_UID" != "$NEW_UID" ]; then
-            echo "Changing UID of $opt_u from $OLD_UID to $NEW_UID"
-            usermod --uid "$NEW_UID" --non-unique "$opt_u"
-            if [ -n "$opt_r" ]; then
-                ${pkgs.coreutils}/bin/find / -xdev -user "$OLD_UID" -exec chown --no-dereference "$opt_u" {} \;
-            fi
-        fi
-
-        OLD_GID=$(${pkgs.getent}/bin/getent group "$opt_g" | cut --field=3 --delimiter=:)
-        NEW_GID=$(stat --format="%g" $VOLUME_AND_WORKDIR)
-
-        if [ "$OLD_GID" != "$NEW_GID" ]; then
-            echo "Changing GID of $opt_g from $OLD_GID to $NEW_GID"
-            groupmod --gid "$NEW_GID" --non-unique "$opt_g"
-            if [ -n "$opt_r" ]; then
-                find / -xdev -group "$OLD_GID" -exec chgrp --no-dereference "$opt_g" {} \;
-            fi
-        fi
-        #exec ${pkgs.gosu.bin}/bin/gosu app_user "$BASH_SOURCE" "$@"
-
-
+        ${pkgs.dockerTools.shadowSetup}
         exec "$@"
-    fi
-    exec "$@"
     '';
 
   ubuntuBase = pkgs.dockerTools.pullImage {
@@ -70,9 +29,12 @@ pkgs.dockerTools.buildImage {
         #!${pkgs.stdenv}
         export PATH=/bin:/usr/bin:/sbin:/usr/sbin:$PATH
         ${pkgs.dockerTools.shadowSetup}
-#        groupadd --gid 5000 app_group
-#        useradd --no-log-init --uid 5000 --gid app_group pedroregispoar
+        groupadd --gid 5000 pedroregispoar_group
+        useradd --no-log-init --uid 5000 --gid pedroregispoar_group pedroregispoar
 
+        # Here the sudoers file is edited
+        # https://stackoverflow.com/a/27355109
+#        ${pkgs.gnused}/bin/sed -i '/wheel/s/^#//g' /etc/sudoers
 
 #        chown root:root /bin/sudo
 #        chmod 777 /bin/sudo
