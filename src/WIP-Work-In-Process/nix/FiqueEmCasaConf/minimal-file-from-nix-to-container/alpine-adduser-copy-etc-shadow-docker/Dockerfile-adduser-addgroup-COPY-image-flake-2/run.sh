4f56aeb616117587e9ead56077bba332f53c81f1
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-25 21:05:47 -0300

    Works

---
+++ src/WIP-Work-In-Process/nix/FiqueEmCasaConf/minimal-file-from-nix-to-container/alpine-adduser-copy-etc-shadow-docker/Dockerfile-adduser-addgroup-COPY-image-flake-2/run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+#set -euxo pipefail
+
+
+IMAGE="gnu-nix-es/$(git rev-parse --short HEAD)"
+VERSION=0.0.1
+IMAGE_VERSION="$IMAGE":"$VERSION"
+
+
+docker build \
+--label org.opencontainers.image.created=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
+--label org.opencontainers.image.revision=$(git rev-parse $(git rev-parse --short HEAD)) \
+--tag \
+"$IMAGE_VERSION" .
+
+
+
+#docker run \
+#--interactive \
+#--tty \
+#--rm \
+#--user pedroregispoar \
+#"$IMAGE_VERSION" sh -c 'nix-env --install --attr nixpkgs.git'
+
+
+docker run \
+--interactive \
+--tty \
+--rm \
+--user pedroregispoar \
+"$IMAGE_VERSION" sh -c 'ls -al home/pedroregispoar/ && echo && env'
---
+++ src/WIP-Work-In-Process/nix/FiqueEmCasaConf/minimal-file-from-nix-to-container/alpine-adduser-copy-etc-shadow-docker/Dockerfile-adduser-addgroup-COPY-image-flake-2/run_apk_update.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+
+# Base ideia from:
+# https://serverfault.com/a/670812
+
+
+
+function get_last_apt_get_update()
+{
+    local aptDate="$(stat -c %Y '/var/cache/apt')"
+    local nowDate="$(date +'%s')"
+
+    echo $((nowDate - aptDate))
+}
+
+function run_apt_get_update()
+{
+    local update_interval="${1}"
+
+    local last_apt_get_update="$(get_last_apt_get_update)"
+
+#    if [[ "$(isEmptyString "${update_interval}")" = 'true' ]]
+#    then
+        # Default To 24 hours
+    update_interval="$((24 * 60 * 60))"
+#    fi
+
+    if [[ "${last_apt_get_update}" -gt "${update_interval}" ]]
+    then
+        echo "apk update"
+        apk update
+        touch '/var/cache/apk'
+    else
+        local last_update="$(date -u -d @"${last_apt_get_update}" +'%-Hh %-Mm %-Ss')"
+
+        echo "Skip apk update because its last run was '${last_update}' ago."
+    fi
+}
+
+
+#last_apt_get_update="$(get_last_apt_get_update)"
+#
+#echo 'get_last_apt_get_update='$(get_last_apt_get_update)
+
+#echo 'get_last_apt_get_update='$(get_last_apt_get_update)
+#
+#echo  "$(date -u -d @"${last_apt_get_update}" +'%-Hh %-Mm %-Ss')"
+#echo  "$(date -u -d @"${last_apt_get_update}" +'%s')"
+#
+#echo "$(date +'%s')"
+#echo "$(date +'%-Hh %-Mm %-Ss')"
+#
+#
+#dt=$(date -u '+%d/%m/%Y %H:%M:%S')
+#echo "$dt"
+#
+#https://stackoverflow.com/a/4182381
+#aptDate="$(stat -c %Y '/var/cache/apt')"
+#
+#echo "$aptDate"
+
+
+# https://unix.stackexchange.com/a/85987
