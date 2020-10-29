5ce1c32eb0f01c0fc2f320689ab8b7ce9ba1cbea
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-29 00:49:48 -0300

    Optmize things

--- src/WIP-Work-In-Process/NYLUG-Graham-Christensen-dockerTools/testing-behaviour/runAsRoot-1/docker-tools-example-1.nix
+++ src/WIP-Work-In-Process/NYLUG-Graham-Christensen-dockerTools/testing-behaviour/runAsRoot-1/docker-tools-example-1.nix
@@ -1,31 +1,25 @@
 let
     pkgs = import <nixpkgs> {};
 
-    nonRootShadowSetup = { user, uid, gid ? uid }: with pkgs; [
+    nonRootShadowSetup = with pkgs; [
           (
           writeTextDir "etc/shadow" ''
             root:!x:::::::
-            ${user}:!:::::::
           ''
           )
           (
           writeTextDir "etc/passwd" ''
             root:x:0:0::/root:${runtimeShell}
-            ${user}:x:${toString uid}:${toString gid}::/home/${user}:
           ''
           )
           (
           writeTextDir "etc/group" ''
             root:x:0:
-            ${user}:x:${toString gid}:
-            sudo:x:27:
           ''
           )
           (
           writeTextDir "etc/gshadow" ''
             root:x::
-            ${user}:x::
-            sudo:x::
           ''
           )
         ];
@@ -58,7 +52,7 @@ pkgs.dockerTools.buildImage {
         file
         sudo
         pam
-    ] ++ nonRootShadowSetup { uid = 999; user = "somebody"; };
+    ] ++ nonRootShadowSetup;
 
     config = {
         Cmd = [ "${pkgs.bashInteractive}/bin/bash" ];
