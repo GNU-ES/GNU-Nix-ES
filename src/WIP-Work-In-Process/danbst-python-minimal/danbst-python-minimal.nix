b2b4f5bcc60d55959b1931098c7525bfdb24055f
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-30 12:28:09 -0300

    Add python3 built with musl S2

---
+++ src/WIP-Work-In-Process/danbst-python-minimal/danbst-python-minimal.nix
@@ -0,0 +1,18 @@
+with import <nixpkgs> {};
+let
+  newPackage = stdenv.mkDerivation {
+    name = python3.name;
+    buildCommand = ''
+      mkdir $out
+      cp -r ${python3}/* $out/
+      chmod +w -R $out
+      find $out | grep -e test -e tkinter -e idle | xargs rm -rf
+      find $out -type f | xargs sed -i "s|${python3}|$out|g"
+      find $out -type l | grep -v -e "include/python" | while read link; do
+        ln -sf $(readlink -f $link | sed "s|${python3}|$out|g") $link
+      done
+      unlink $out/include/python3.7
+      ln -sf $out/include/python3.7m $out/include/python3.7
+    '';
+  };
+in newPackage
