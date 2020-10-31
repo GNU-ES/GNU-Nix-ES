4d6bac99cecc791f6248b9a977177ec5186c52a2
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-31 18:34:35 -0300

    Add docker nix-build

--- src/WIP-Work-In-Process/NixOS-tests/leveraging-nixos-tests-in-your-project-1/run.sh
+++ src/WIP-Work-In-Process/NixOS-tests/leveraging-nixos-tests-in-your-project-1/run.sh
@@ -5,3 +5,14 @@ set -euxo pipefail
 
 
 nix-build
+
+
+# TODO: use an minimal flake docker image to do this and use a volume to /nix!
+docker run \
+--interactive \
+--privileged \
+--rm \
+--tty \
+--volume "$(pwd)":/code \
+--workdir /code \
+nixos/nix:latest nix-build
