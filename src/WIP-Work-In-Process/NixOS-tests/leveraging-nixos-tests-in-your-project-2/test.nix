5b6ed4bdea113f13fa2dfc27f18f1019d40758fe
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-31 18:37:06 -0300

    Works

--- src/WIP-Work-In-Process/NixOS-tests/leveraging-nixos-tests-in-your-project-2/test.nix
+++ src/WIP-Work-In-Process/NixOS-tests/leveraging-nixos-tests-in-your-project-2/test.nix
@@ -26,18 +26,16 @@
         };
     };
 
-    testScript = ''
-        start_all()
+    testScript = with pkgs.dockerTools; ''
+        unix_time_second1 = "1970-01-01T00:00:01Z"
         docker.wait_for_unit("sockets.target")
-        docker.succeed("tar cv --files-from /dev/null | docker import - scratchimg")
-        docker.succeed(
-            "docker run -d --name=sleeping -v /nix/store:/nix/store -v /run/current-system/sw/bin:/bin scratchimg /bin/sleep 10"
-        )
-        docker.succeed("docker ps | grep sleeping")
-        docker.succeed("sudo -u hasprivs docker ps")
-        docker.fail("sudo -u noprivs docker ps")
-        docker.succeed("docker stop sleeping")
-        # Must match version 4 times to ensure client and server git commits and versions are correct
-        docker.succeed('[ $(docker version | grep ${pkgs.docker.version} | wc -l) = "4" ]')
+        with subtest("Ensure Docker images use a stable date by default"):
+            docker.succeed(
+                "docker load --input='${examples.bash}'"
+            )
+            assert unix_time_second1 in docker.succeed(
+                "docker inspect ${examples.bash.imageName} "
+                + "| ${pkgs.jq}/bin/jq -r .[].Created",
+            )
     '';
 }
