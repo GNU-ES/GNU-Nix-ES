4d3a4f916a82fa2cb7f5c83f9ca686ff3cec90d1
Pedro Regis <pedroalencarregis@hotmail.com>
2020-11-02 03:52:41 -0300

    adduser-addgroup-sudo-wheel-COPY-alpine-dockerTools- --login-3-4

---
+++ src/WIP-Work-In-Process/nix/FiqueEmCasaConf/minimal-file-from-nix-to-container/Dockerfile-adduser-addgroup-sudo-wheel-COPY-alpine-dockerTools- --login-3-4/run.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env sh
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+#set -euxo pipefail
+
+
+IMAGE="gnu-nix-es/$(git rev-parse --short HEAD)"
+VERSION=0.0.1
+
+TARGET_0='builder'
+TARGET_1='prod'
+
+IMAGE_VERSION_TARGET_0="$IMAGE""$TARGET_0":"$VERSION"
+IMAGE_VERSION_TARGET_1="$IMAGE""$TARGET_1":"$VERSION"
+
+
+nix-build './nix-Luca Bruno-dockerTools.nix'
+
+docker load < ./result
+
+docker run \
+--interactive \
+--tty \
+--rm \
+--workdir /code \
+--volume "$(pwd)":/code \
+--user=pedroregispoar \
+alpine-gnu-nix-es:0.0.1 bash -c 'stat --format="%a" /sbin/sudo'
+
+
+docker run \
+--interactive \
+--tty \
+--rm \
+--workdir /code \
+--volume "$(pwd)":/code \
+alpine-gnu-nix-es:0.0.1 bash -c 'chmod 4511 /sbin/sudo && su pedroregispoar -c 'id''
+
+
+docker run \
+--interactive \
+--tty \
+--rm \
+--workdir /code \
+--volume "$(pwd)":/code \
+alpine-gnu-nix-es:0.0.1 bash -c 'echo "Set disable_coredump false" >> /etc/sudo.conf && chmod 4511 /sbin/sudo && stat --format="%a" /sbin/sudo'
