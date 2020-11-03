2fe6ebce43cac55983f455e88bd6fb8df4836367
Pedro Regis <pedroalencarregis@hotmail.com>
2020-11-03 02:07:29 -0300

    Saving

---
+++ src/WIP-Work-In-Process/build-vm/nixos-superpowers/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -eux pipefail
+
+
+export NIX_PATH=nixos-config=$PWD/configuration.nix:nixpkgs=channel:nixos-19.09
+
+nixos-rebuild build-vm
+
+#nixos-rebuild -I nixos-config="$(pwd)"/configuration.nix build-vm
+
+./result/bin/run-nixos-vm
+
+
