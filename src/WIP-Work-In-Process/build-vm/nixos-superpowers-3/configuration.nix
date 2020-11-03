8cfbee820cfafbdc5e266266d2438353c4de4023
Pedro Regis <pedroalencarregis@hotmail.com>
2020-11-03 13:45:57 -0300

    Add example with allowUnfree

---
+++ src/WIP-Work-In-Process/build-vm/nixos-superpowers-3/configuration.nix
@@ -0,0 +1,23 @@
+{ pkgs, config, ... }: {
+
+    users = {
+        extraUsers.root.initialHashedPassword = "";
+        mutableUsers = false;
+
+         extraUsers.pedro= {
+            shell = pkgs.zsh;
+            extraGroups = [ "root" ];
+            };
+    };
+
+    nix = {
+        package = pkgs.nixUnstable;
+        extraOptions = ''
+            experimental-features = nix-command flakes
+        '';
+    };
+
+    # Allow unfree packages :[
+    nixpkgs.config.allowUnfree = true;
+
+}
