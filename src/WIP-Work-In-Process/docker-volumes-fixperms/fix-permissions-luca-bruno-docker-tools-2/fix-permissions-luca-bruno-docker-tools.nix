0cbba4523bcffe6c712935296463ac938c96a67e
Pedro Regis <pedroalencarregis@hotmail.com>
2020-11-07 17:15:28 -0300

    Adds fix-permissions-luca-bruno-docker-tools-2

---
+++ src/WIP-Work-In-Process/docker-volumes-fixperms/fix-permissions-luca-bruno-docker-tools-2/fix-permissions-luca-bruno-docker-tools.nix
@@ -0,0 +1,74 @@
+let
+    pkgs = import <nixpkgs> {};
+
+    new_user = "app_user";
+
+    entrypoint = pkgs.writeScript "entrypoin-file.sh" ''
+        #!${pkgs.stdenv.shell}
+        ${pkgs.dockerTools.shadowSetup}
+
+        set -e
+
+        # allow the container to be started with `--user`
+        if [ "$(${pkgs.coreutils}/bin/id --user)" = "0" ]; then
+
+            NEW_USER_NAME=${new_user}
+            NEW_GROUP_NAME='app_group'
+            VOLUME_AND_WORKDIR='/code'
+
+            #
+            #groupadd --gid 0 root
+            #useradd --no-log-init --uid 0 --gid root root
+
+            groupadd --gid 56789 "$NEW_GROUP_NAME"
+            useradd --no-log-init --uid 12345 --gid "$NEW_GROUP_NAME" "$NEW_USER_NAME"
+
+            OLD_USER_ID=$( ${pkgs.getent}/bin/getent passwd "$NEW_USER_NAME" | cut --field=3 --delimiter=:)
+            NEW_USER_ID=$(stat --format="%u" "$VOLUME_AND_WORKDIR")
+
+            if [ "$OLD_USER_ID" != "$NEW_USER_ID" ]; then
+                echo "Changing uid (user identifier) of "$NEW_USER_NAME" from $OLD_USER_ID to $NEW_USER_ID"
+                usermod --uid "$NEW_USER_ID" --non-unique "$NEW_USER_NAME"
+
+                ${pkgs.findutils}/bin/find / -xdev -user "$OLD_USER_ID" -exec chown --no-dereference "$NEW_USER_NAME" {} \;
+            fi
+
+            OLD_GROUP_ID=$(${pkgs.getent}/bin/getent group "$NEW_GROUP_NAME" | cut --field=3 --delimiter=:)
+            NEW_GROUP_ID=$(stat --format="%g" $VOLUME_AND_WORKDIR)
+
+            if [ "$OLD_GROUP_ID" != "$NEW_GROUP_ID" ]; then
+                echo "Changing gid (group identifier) of "$NEW_GROUP_NAME" from $OLD_GROUP_ID to $NEW_GROUP_ID"
+                groupmod --gid "$NEW_GROUP_ID" --non-unique "$NEW_GROUP_NAME"
+
+                ${pkgs.findutils}/bin/find / -xdev -group "$OLD_GROUP_ID" -exec chgrp --no-dereference "$NEW_GROUP_NAME" {} \;
+            fi
+            exec ${pkgs.gosu.bin}/bin/gosu "$NEW_USER_NAME" "$BASH_SOURCE" "$@"
+        fi
+        exec "$@"
+    '';
+in
+
+pkgs.dockerTools.buildImage {
+
+    name = "fix-permissions-luca-bruno-docker-tools";
+    tag = "0.0.1";
+    created = "now";
+
+#    runAsRoot = ''
+#        #!${pkgs.stdenv}
+#        ${pkgs.dockerTools.shadowSetup}
+#        groupadd --gid 56789 app_group
+#        useradd --no-log-init --uid 12345 --gid app_group app_user
+#    '';
+
+    contents = with pkgs; [
+        bashInteractive
+        coreutils
+        which
+       ];
+
+    config = {
+        Cmd = [ "${pkgs.bashInteractive}/bin/bash" ];
+        Entrypoint = [ entrypoint ];
+    };
+}
---
+++ src/WIP-Work-In-Process/docker-volumes-fixperms/fix-permissions-luca-bruno-docker-tools-2/run.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+IMAGE="gnu-nix-es/$(git rev-parse --short HEAD)"
+VERSION=0.0.1
+IMAGE_VERSION="$IMAGE":"$VERSION"
+
+
+#docker build \
+#--label org.opencontainers.image.created=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
+#--label org.opencontainers.image.revision=$(git rev-parse $(git rev-parse --short HEAD)) \
+#--tag \
+#"$IMAGE_VERSION" .
+#
+#docker run \
+#--interactive \
+#--tty \
+#--rm \
+#--user pedro \
+#"$IMAGE_VERSION" \
+#sh
+
+
+nix-build './fix-permissions-luca-bruno-docker-tools.nix'
+
+docker load < ./result
+
+docker run \
+--interactive \
+--tty \
+--rm \
+--workdir /code \
+--volume "$(pwd)":/code \
+fix-permissions-luca-bruno-docker-tools:0.0.1 \
+bash -c 'id'
+
+docker run \
+--interactive \
+--tty \
+--rm \
+--workdir /code \
+--user 0 \
+--volume "$(pwd)":/code \
+fix-permissions-luca-bruno-docker-tools:0.0.1 \
+bash -c 'id'
+
+#docker run \
+#--interactive \
+#--tty \
+#--rm \
+#--workdir /code \
+#--user root \
+#--volume "$(pwd)":/code \
+#fix-permissions-luca-bruno-docker-tools:0.0.1 \
+#bash -c 'id'
+
+
+#docker run \
+#--interactive \
+#--tty \
+#--rm \
+#--user app_user \
+#fix-permissions-luca-bruno-docker-tools:0.0.1 \
+#bash -c 'id'
+
+
+echo 'The result of id in the current machine:'
+id
+
+docker run \
+--interactive \
+--tty \
+--rm \
+--workdir /code \
+--volume "$(pwd)":/code \
+fix-permissions-luca-bruno-docker-tools:0.0.1 \
+bash -c 'touch some_file.txt && echo && stat some_file.txt'
+
+echo
+
+stat some_file.txt
+
+rm some_file.txt
+
+# Create some tests, use maybe stat or some thing like to be sure it works!
+# Inside the runing container?
+# touch some_file.txt
+# Outside of it run a non sudo rm in this file?
+
