4fbabd0360dce072647065ff43545bc576f0ef4c
Pedro Regis <pedroalencarregis@hotmail.com>
2020-12-06 18:36:41 -0300

    Add working example

---
+++ src/WIP-Work-In-Process/docker-volumes-fixperms/fix-permissions-luca-bruno-docker-tools-AsRoot-1/README.md
@@ -0,0 +1,22 @@
+#
+
+
+[ ] find a way to be [DRY](TODO: add a link here) in the user of:
+    [ ] /code used in WorkingDir, Volumes
+    [ ] app_user and app_group 
+    [ ] the entrypoint file name
+    
+
+What are the differences in doing it in the entrypoint or in the runAsRoot?
+groupadd --gid 5000 app_group
+useradd --no-log-init --uid 5000 --gid app_group app_user
+
+
+--workdir /code \
+--volume "$(pwd)":/code \
+
+WorkingDir = "/code";
+
+Volumes = {
+  "/code" = {};
+};
---
+++ src/WIP-Work-In-Process/docker-volumes-fixperms/fix-permissions-luca-bruno-docker-tools-AsRoot-1/fix-permissions-luca-bruno-docker-tools-AsRoot.nix
@@ -0,0 +1,69 @@
+let
+    pkgs = import <nixpkgs> {};
+
+    new_user_name = "app_user";
+    new_user_group = "app_group";
+    volume_and_workdir = "/code";
+
+    entrypoint = pkgs.writeScript "entrypoin-file.sh" ''
+        #!${pkgs.stdenv.shell}
+        ${pkgs.dockerTools.shadowSetup}
+
+        set -e
+
+        # allow the container to be started with `--user`
+        if [ "$(${pkgs.coreutils}/bin/id --user)" = "0" ]; then
+
+            NEW_USER_NAME=${new_user_name}
+            NEW_GROUP_NAME=${new_user_group}
+            VOLUME_AND_WORKDIR=${volume_and_workdir}
+
+            OLD_USER_ID=$( ${pkgs.getent}/bin/getent passwd "$NEW_USER_NAME" | cut --field=3 --delimiter=:)
+            NEW_USER_ID=$(stat --format="%u" "$VOLUME_AND_WORKDIR")
+
+            if [ "$OLD_USER_ID" != "$NEW_USER_ID" ]; then
+                echo "Changing uid (user identifier) of "$NEW_USER_NAME" from $OLD_USER_ID to $NEW_USER_ID"
+                usermod --uid "$NEW_USER_ID" --non-unique "$NEW_USER_NAME"
+
+                ${pkgs.findutils}/bin/find / -xdev -user "$OLD_USER_ID" -exec chown --no-dereference "$NEW_USER_NAME" {} \;
+            fi
+
+            OLD_GROUP_ID=$(${pkgs.getent}/bin/getent group "$NEW_GROUP_NAME" | cut --field=3 --delimiter=:)
+            NEW_GROUP_ID=$(stat --format="%g" $VOLUME_AND_WORKDIR)
+
+            if [ "$OLD_GROUP_ID" != "$NEW_GROUP_ID" ]; then
+                echo "Changing gid (group identifier) of "$NEW_GROUP_NAME" from $OLD_GROUP_ID to $NEW_GROUP_ID"
+                groupmod --gid "$NEW_GROUP_ID" --non-unique "$NEW_GROUP_NAME"
+
+                ${pkgs.findutils}/bin/find / -xdev -group "$OLD_GROUP_ID" -exec chgrp --no-dereference "$NEW_GROUP_NAME" {} \;
+            fi
+            exec ${pkgs.gosu}/bin/gosu "$NEW_USER_NAME" "$BASH_SOURCE" "$@"
+        fi
+        exec "$@"
+    '';
+in
+
+pkgs.dockerTools.buildImage {
+
+    name = "fix-permissions-luca-bruno-docker-tools-asroot";
+    tag = "0.0.1";
+    created = "now";
+
+    runAsRoot = ''
+        #!${pkgs.stdenv}
+        ${pkgs.dockerTools.shadowSetup}
+        groupadd --gid 56789 app_group
+        useradd --no-log-init --uid 12345 --gid app_group app_user
+    '';
+
+    contents = with pkgs; [
+        bashInteractive
+        coreutils
+        which
+       ];
+
+    config = {
+        Cmd = [ "${pkgs.bashInteractive}/bin/bash" ];
+        Entrypoint = [ entrypoint ];
+    };
+}
