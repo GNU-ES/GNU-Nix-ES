34a284cb57d3a2ae286db132de9354528679140a
Pedro Regis <pedroalencarregis@hotmail.com>
2021-01-06 22:33:42 -0300

    Improve flake example

--- src/WIP-Work-In-Process/install-flake/Nix-Friday-flakes/ex-0/run.sh
+++ src/WIP-Work-In-Process/install-flake/Nix-Friday-flakes/ex-0/run.sh
@@ -3,6 +3,10 @@
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+echo 'result' >> '.gitignore'
+
 git init
 
 git add .
@@ -10,13 +14,24 @@ git add .
 git commit --message 'Save the inicial repository state'
 
 nix flake init
+git add .
+
+git commit --message 'Save flake state 1'
+
+nix flake update
+#
+#nix run --commit-lock-file
 
 git add .
 
-git commit --message 'Save flake state'
+git commit --message 'Save flake state 2'
+
+#git status
 
 nix build
 
+#git status
+
 nix run
 
-sudo rm --recursive .git result flake.{lock,nix}
+sudo rm --recursive .git result flake.{lock,nix} '.gitignore'
