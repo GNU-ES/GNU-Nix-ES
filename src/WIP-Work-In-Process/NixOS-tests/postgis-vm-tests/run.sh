7a66f33524248ee03f34a9e62023e43faeb8c05b
Pedro Regis <pedroalencarregis@hotmail.com>
2021-01-13 20:09:25 -0300

    Adds usefull copy paste command

--- src/WIP-Work-In-Process/NixOS-tests/postgis-vm-tests/run.sh
+++ src/WIP-Work-In-Process/NixOS-tests/postgis-vm-tests/run.sh
@@ -5,3 +5,5 @@ set -euxo pipefail
 
 
 nix-build
+
+#nix-shell -I nixpkgs=channel:nixos-20.09 --packages nixFlakes --run 'nix shell nixpkgs#wget nixpkgs#man nixpkgs#man-db nixpkgs#qemu nixpkgs#stdenv --command ./run.sh'
