4860a30771d841190e6066afa6bda25ed40c9af9
Pedro Regis <pedroregispoar@gmail.com>
2021-01-14 00:02:44 -0300

    Updates sha of cloud image

--- src/WIP-Work-In-Process/ubuntu-18-04-vm-QEMU-nix-cloud-images/default.nix
+++ src/WIP-Work-In-Process/ubuntu-18-04-vm-QEMU-nix-cloud-images/default.nix
@@ -7,7 +7,7 @@ in
 rec {
   image = pkgs.fetchurl {
     url = "https://cloud-images.ubuntu.com/releases/18.04/release/${img_orig}";
-    sha256 = "0qzpii17aii32232l9rrn3xkxwwd0h10gpafj5h44mv44wa42laf";
+    sha256 = "0qkpjn9ks8rrq0jp1dx9zv1c0jknam1vw87dqw093wzp45qjs2qr";
   };
 
   # This is the cloud-init config
