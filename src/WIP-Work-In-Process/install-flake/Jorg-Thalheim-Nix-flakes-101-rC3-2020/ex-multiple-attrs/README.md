0cf5435890201f542fd749e164f1faf1d54419c0
Your Name <you@example.com>
2021-01-18 18:36:43 -0300

    First try of multiple attrs

---
+++ src/WIP-Work-In-Process/install-flake/Jorg-Thalheim-Nix-flakes-101-rC3-2020/ex-multiple-attrs/README.md
@@ -0,0 +1,6 @@
+
+
+This requires flake working to be ran.
+
+TODO:
+- build the attr with what is desired
---
+++ src/WIP-Work-In-Process/install-flake/Jorg-Thalheim-Nix-flakes-101-rC3-2020/ex-multiple-attrs/flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Hello world";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+    let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+        packages.x86_64-linux.example39 = pkgs.python39;
+        packages.x86_64-linux.example38 = pkgs.python38;
+        defaultPackage.x86_64-linux = self.packages.x86_64-linux.example38;
+  };
+}
