dd2e7afdffc757211bf9a5cd807749b34c73b5c8
Your Name <you@example.com>
2021-01-19 21:34:23 -0300

    Cleaning a bit

--- src/WIP-Work-In-Process/install-flake/TROPIN-Flakes-in-nix-3.0/ex-4-numtild-dockerTools-redis/default.nix
+++ src/WIP-Work-In-Process/install-flake/TROPIN-Flakes-in-nix-3.0/ex-4-numtild-dockerTools-redis/default.nix
@@ -1,9 +1,6 @@
 { pkgs ?  import <nixpkgs> {} }:
-let
-    redisMinimal = import ./redis-minimal.nix { inherit pkgs; };
-in
-    pkgs.dockerTools.buildLayeredImage {    # helper to build docker image
-    name = "nix-redis-dockertools";             # give docker image a name
-    tag = "0.0.1";                          # provide a tag
-    contents = [ pkgs.redis ];              # use redis package
+pkgs.dockerTools.buildLayeredImage {    # helper to build docker image
+  name = "nix-redis-dockertools";       # give docker image a name
+  tag = "0.0.1";                        # provide a tag
+  contents = [ pkgs.redis ];            # use redis package
 }
