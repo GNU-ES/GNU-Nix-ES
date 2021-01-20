5707b84fde85de620596ccd2f28b90d37c775673
Your Name <you@example.com>
2021-01-19 22:44:18 -0300

    Adds podman rootless example, broken in DinD

---
+++ src/WIP-Work-In-Process/install-flake/podman-rootless/test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+
+# TODO: Why not only
+# sudo echo pedroregispoar:10000:65536 >> /etc/sub?id
+#
+sudo su -c 'echo pedroregispoar:10000:65536 >> /etc/subuid'
+sudo su -c 'sudo echo pedroregispoar:10000:65536 >> /etc/subgid'
+
+podman \
+run \
+--interactive \
+--net=host \
+--tty \
+--rm \
+--workdir /code \
+--volume "$(pwd)":/code \
+docker.io/library/alpine:3.13.0 \
+sh -c 'apk add --no-cache git && git init'
