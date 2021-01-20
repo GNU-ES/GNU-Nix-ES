241e7fa8fdaef5c58d59efec6358e8f7c7a53f12
Your Name <you@example.com>
2021-01-20 14:03:38 -0300

    Minor refactor

--- src/WIP-Work-In-Process/install-flake/TROPIN-Flakes-in-nix-3.0/ex-4-numtild-dockerTools-multiple-packages/run.sh
+++ src/WIP-Work-In-Process/install-flake/TROPIN-Flakes-in-nix-3.0/ex-4-numtild-dockerTools-multiple-packages/run.sh
@@ -30,10 +30,16 @@ nix build --out-link oci_image
 sudo docker load < ./oci_image
 
 
+# nix shell nixpkgs#{docker,neovim}
+
 sudo docker \
 run \
 --interactive \
 --tty \
-nix-dockertools-multiple-packages:0.0.1 bash -c 'hello | figlet'
+--workdir /code \
+--volume "$(pwd)":/code \
+nix-dockertools-multiple-packages:0.0.1 -c './test.sh'
 
-sudo rm --force --recursive .git .gitignore flake.lock result oci_image
+
+sudo rm --force --recursive .git
+rm --force --recursive .gitignore flake.lock result oci_image
---
+++ src/WIP-Work-In-Process/install-flake/TROPIN-Flakes-in-nix-3.0/ex-4-numtild-dockerTools-multiple-packages/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+
+hello --version
+figlet --version
+
+hello | figlet
