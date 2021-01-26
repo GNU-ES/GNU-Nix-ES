d83c2ca8b20eacc8f080dc32b4b3cb0dc1e1c1a6
Your Name <you@example.com>
2021-01-26 02:00:24 -0300

    Saving state

---
+++ src/WIP-Work-In-Process/install-flake/TROPIN-Flakes-in-nix-3.0/ex-4-numtild-dockerTools-poetry2nix-multiple-outputs/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A usefull description";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+
+    packages.myExampleFlake = import ./default.nix {
+      pkgs = nixpkgs.legacyPackages.${system};
+    };
+
+    packages.myExampleFlake2 = import ./default2.nix {
+      pkgs = nixpkgs.legacyPackages.${system};
+    };
+
+    defaultPackage = self.packages.${system}.myExampleFlake;
+    packages = self.packages.${system}.myExampleFlake2;
+  });
+
+}
