12da353bca72088fc92fcc890584da5e0d9f7a13
Your Name <you@example.com>
2021-01-26 02:00:04 -0300

    Adds example of toybox

--- src/WIP-Work-In-Process/install-flake/podman-rootless/test_podman-rootless-wrapper.sh
+++ src/WIP-Work-In-Process/install-flake/podman-rootless/test_podman-rootless-wrapper.sh
@@ -4,6 +4,15 @@
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -eux pipefail
 
+
+podman \
+run \
+--interactive \
+--rm \
+--runtime $(which runc) \
+docker.io/tianon/toybox \
+sh -c id
+
 podman \
 run \
 --interactive \
