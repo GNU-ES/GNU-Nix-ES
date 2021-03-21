20edda8a889c06930b5186f48cb13018b7120f42
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-03-21 17:11:04 -0300

    Adds WIP example of volume permission Dimchansky

---
+++ src/WIP-Work-In-Process/docker-volumes-fixperms/Dimchansky-solution/README.md
@@ -0,0 +1,7 @@
+#
+
+TODO: study this, make tests all this sort of stuff
+https://stackoverflow.com/a/29350174
+
+
+sudo chown --recursive pedro:users .
---
+++ src/WIP-Work-In-Process/docker-volumes-fixperms/Dimchansky-solution/run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+
+rmdir artifacts
+mkdir artifacts
+
+podman \
+run \
+--interactive=true \
+--tty=false \
+--rm=true \
+--workdir=/artifacts \
+--volume="$(pwd)"/artifacts:/artifacts \
+docker.io/library/alpine:3.13.0 \
+sh \
+<< COMMANDS
+echo Changing owner from \$(id -u):\$(id -g) to $(id -u):$(id -g)
+chown -R $(id -u):$(id -g) /artifacts
+COMMANDS
+
+
+stat artifacts
+
+rmdir artifacts
